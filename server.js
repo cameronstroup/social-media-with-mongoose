168d63afeb67646e6c7ce35a87c7606c41bf1a00
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-06-09 20:21:51 -0500

    cant get server to run

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
+
 app.use(require("./routes"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/socialDB",
+  process.env.MONGODB_URI || "mongodb://localhost/social-network",
   {
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
 
-// Use this to log mongo queries being executed!n
+// Use this to log mongo queries being executed
 mongoose.set("debug", true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
