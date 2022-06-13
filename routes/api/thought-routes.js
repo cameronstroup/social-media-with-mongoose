b1438ee10f6e4dc6be3fa66ae5e145f2212db8c2
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-06-12 19:49:01 -0500

    added routes but now its not working

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,10 +3,21 @@ const {
   getAllThoughts,
   getThoughtById,
   createThought,
-  addReaction,
   updateThought,
   deleteThought,
+  addReaction,
   deleteReaction,
 } = require("../../controllers/thought-controller");
+const { route } = require("./user-routes");
 
-module.exports = router;
+router.route("/").get(getAllThoughts);
+
+router.route("/:userId").post(createThought);
+
+router.route("/:thoughtId").get(getThoughtById).put(updateThought);
+
+router.route("/:userId/:thoughtId").delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,17 +1,20 @@
 const router = require("express").Router();
-
 const {
   getAllUsers,
   getUserById,
   createUser,
   updateUser,
   deleteUser,
-} = require("../../controllers/User-controller");
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/user-controller");
 
-// Set up GET all and POST at /api/Users
+router.route("/").get().post();
+
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route("/:id/friends/:friendId").post(addFriend).delete(deleteFriend);
+
 router.route("/").get(getAllUsers).post(createUser);
-
-// Set up GET one, PUT, and DELETE at /api/Users/:id
-router.route("/:id").get(getUserById).delete(deleteUser);
 
 module.exports = router;
